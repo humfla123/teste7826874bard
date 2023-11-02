c0284c50c043d436cd961e8126da022f2e0c39b5
humfla123 <88781888+humfla123@users.noreply.github.com>
2023-11-02 14:36:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,12 +6,15 @@ API_KEY = "JwYLxfbojMR2LGOwBxGoBA"
 # O modelo de linguagem que você deseja usar
 MODEL_NAME = "KoboldAI"
 
-# O texto que você deseja gerar
-PROMPT = "Escreva um poema sobre um gato."
+# O código que você deseja gerar
+PROMPT = """def greet_world():
+  print("Hello, world!")
 
-# Faça uma solicitação à API do KoboldAI para gerar texto
+greet_world()"""
+
+# Faça uma solicitação à API do KoboldAI para gerar código
 response = requests.post(
-    "https://api.koboldai.com/generate-text",
+    "https://api.koboldai.com/generate-code",
     headers={"Authorization": f"Bearer {API_KEY}"},
     json={"model_name": MODEL_NAME, "prompt": PROMPT},
 )
@@ -19,10 +22,10 @@ response = requests.post(
 # Verifique o status da solicitação
 if response.status_code == 200:
     # A solicitação foi bem-sucedida
-    generated_text = response.json()["generated_text"]
+    generated_code = response.json()["generated_code"]
 
-    # Imprima o texto gerado
-    print(generated_text)
+    # Execute o código gerado
+    exec(generated_code)
 else:
     # A solicitação falhou
-    print("Falha ao gerar texto:", response.status_code)
+    print("Falha ao gerar código:", response.status_code)
